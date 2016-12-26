f17878d58935d742dddd2f2e365ca67599fabb05
stefunk <kirikakis@gmail.com>
2016-12-26 23:34:31 +0200

    Adding role kubernetes

---
+++ roles/kubernetes/files/installK8sUbuntu.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
